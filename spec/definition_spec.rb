7d8b0a57b634fc5bdbc16168250f9802726d5869
Noah Horwitz <nhhor@post.com>
2019-12-20 12:07:58 -0800

    complete code for spec 4, update all views and tests per template. tests pass, error on definition.erb still.

---
+++ spec/definition_spec.rb
@@ -0,0 +1,94 @@
+require 'rspec'
+require 'definition'
+require 'word'
+require 'pry'
+
+describe '#Definition' do
+
+  before(:each) do
+    Word.clear()
+    Definition.clear()
+    @word = Word.new("Giant Steps", nil)
+    @word.save()
+  end
+
+  describe('#==') do
+    it("is the same definition if it has the same attributes as another definition") do
+      definition = Definition.new("Naima", @word.id, nil)
+      definition2 = Definition.new("Naima", @word.id, nil)
+      expect(definition).to(eq(definition2))
+    end
+  end
+
+  describe('.all') do
+    it("returns a list of all definitions") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("Naima", @word.id, nil)
+      definition2.save()
+      expect(Definition.all).to(eq([definition, definition2]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all definitions") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("Naima", @word.id, nil)
+      definition2.save()
+      Definition.clear()
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("saves a definition") do
+      definition = Definition.new("Naima", @word.id, nil)
+      definition.save()
+      expect(Definition.all).to(eq([definition]))
+    end
+  end
+
+  describe('.find') do
+    it("finds a definition by id") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("Naima", @word.id, nil)
+      definition2.save()
+      expect(Definition.find(definition.id)).to(eq(definition))
+    end
+  end
+
+  describe('#update') do
+    it("updates an definition by id") do
+      definition = Definition.new("Naima", @word.id, nil)
+      definition.save()
+      definition.update("Mr. P.C.", @word.id)
+      expect(definition.name).to(eq("Mr. P.C."))
+    end
+  end
+
+  describe('#delete') do
+    it("deletes an definition by id") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("Naima", @word.id, nil)
+      definition2.save()
+      definition.delete()
+      expect(Definition.all).to(eq([definition2]))
+    end
+  end
+
+  describe('.find_by_word') do
+    it("finds definitions for an word") do
+      word2 = Word.new("Blue", nil)
+      word2.save
+      definition = Definition.new("text1", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("text2", word2.id , nil)
+      definition2.save()
+      expect(Definition.find_by_word(word2.id)).to(eq([definition2]))
+    end
+  end
+
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,46 +1,114 @@
-require('rspec')
-require('word.rb')
+require("rspec")
+require("word")
 
 describe '#Word' do
 
-    before(:each) do
-        Word.clear()
-    end
-
-    describe("(1) #==") do
-        it("is the same word if it has the same attributes as another word") do
-          word1 = Word.new("Test 1", nil)
-          word2 = Word.new("Test 1", nil)
-          expect(word1).to(eq(word2))
-        end
-      end
-  
-      describe("(2) #save") do
-        it("saves a word") do
-          word1 = Word.new("Test 3", nil) # nil added as second argument
-          word1.save()
-          word2 = Word.new("Test 3", nil) # nil added as second argument
-          word2.save()
-          expect(Word.all).to(eq([word1, word2]))
-        end
-      end
-
-      describe("(3) .all") do
-        it("returns an empty array when there are no words") do
-          expect(Word.all()).to(eq([]))
-        end
-      end
-          
-      describe("(4) .clear") do
-        it("clears all words") do
-          word1 = Word.new("Test 4", nil)
-          word1.save()
-          word2 = Word.new("Test 4", nil)
-          word2.save()
-          Word.clear()
-          expect(Word.all).to(eq([]))
-        end
-      end
-
-      
+  before(:each) do
+    Word.clear()
+    Definition.clear()
   end
+
+  describe('(1) #==') do
+    it("is the same word if it has the same attributes as another word") do
+      word1 = Word.new("Blue", nil)
+      word2 = Word.new("Blue", nil)
+      expect(word1).to(eq(word2))
+    end
+  end
+
+  describe("(2) .all") do
+    it("returns an empty array when there are no words") do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('(3) #save') do
+    it("saves an word") do
+      word1 = Word.new("Giant Steps", nil)
+      word1.save()
+      word2 = Word.new("Blue", nil)
+      word2.save()
+      expect(Word.all).to(eq([word1, word2]))
+    end
+  end
+
+  describe('(4) .clear') do
+    it("clears all words") do
+      word1 = Word.new("Giant Steps", nil)
+      word1.save()
+      word2 = Word.new("Blue", nil)
+      word2.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('(5) .find') do
+    it("finds an word by id") do
+      word1 = Word.new("Giant Steps", nil)
+      word1.save()
+      word2 = Word.new("Blue", nil)
+      word2.save()
+      expect(Word.find(word1.id)).to(eq(word1))
+    end
+  end
+
+  describe('(6) #update') do
+    it("updates an word by id") do
+      word1 = Word.new("Giant Steps", nil)
+      word1.save()
+      word1.update("A Love Supreme")
+      expect(word1.name).to(eq("A Love Supreme"))
+    end
+  end
+
+  describe('(7) #delete') do
+    it("deletes an word by id") do
+      word1 = Word.new("Giant Steps", nil)
+      word1.save()
+      word2 = Word.new("Blue", nil)
+      word2.save()
+      word1.delete()
+      expect(Word.all).to(eq([word2]))
+    end
+  end
+
+  describe('(8) .search') do
+    it("Searches for matching words by name") do
+      word1 = Word.new("Giant Steps", nil)
+      word1.save()
+      word2 = Word.new("Blue", nil)
+      word2.save()
+      word3 = Word.new("Green Steps", nil)
+      word3.save()
+
+      expect(Word.search("Steps")).to(eq([word1,word3]))
+    end
+  end
+
+  describe('(9) .sort') do
+    it('sorts by alphebetical order') do
+    word1 = Word.new("Giant Steps", nil)
+    word1.save()
+    word2 = Word.new("Blue", nil)
+    word2.save()
+    word3 = Word.new("Green Steps", nil)
+    word3.save()
+    expect(Word.sort()).to(eq([word2, word1, word3]))
+  end
+end
+
+describe('(10) #definitions') do
+    it("returns an word's definitions") do
+      word1 = Word.new("Giant Steps", nil)
+      word1.save()
+      definition = Definition.new("Naima", word1.id, nil)
+      definition.save()
+      definition2 = Definition.new("Cousin Mary", word1.id, nil)
+      definition2.save()
+      expect(word1.definitions).to(eq([definition, definition2]))
+    end
+  end
+
+
+end
