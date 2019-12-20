52ad753e4db54a28edd9449677a6b0eb881a8b9c
Noah Horwitz <nhhor@post.com>
2019-12-20 14:11:00 -0800

    update integration specs and code, all tests passing.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,23 +3,23 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('create an word path', {:type => :feature}) do
+describe('create a word path', {:type => :feature}) do
   it('creates an word and then goes to the word page') do
     visit('/words')
     click_on('Add a new word')
-    fill_in('word_name', :with => 'Yellow Submarine')
+    fill_in('word_name', :with => 'test_word')
     click_on('Go!')
-    expect(page).to have_content('Yellow Submarine')
+    expect(page).to have_content('test_word')
   end
 end
 
 describe('create a definition path', {:type => :feature}) do
     it('creates an word and then goes to the word page') do
-      word = Word.new("Yellow Submarine", nil)
+      word = Word.new("test_word", nil)
       word.save
       visit("/words/#{word.id}")
-      fill_in('definition_name', :with => 'All You Need Is Love')
+      fill_in('definition_name', :with => 'test_definition')
       click_on('Add definition')
-      expect(page).to have_content('All You Need Is Love')
+      expect(page).to have_content('test_definition')
     end
   end
