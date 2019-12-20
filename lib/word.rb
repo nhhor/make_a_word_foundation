1ff4ea85118a27424305efeef38b7a228109d43b
Noah Horwitz <nhhor@post.com>
2019-12-20 09:21:47 -0800

    update tests and word.rb per template.

---
+++ lib/word.rb
@@ -0,0 +1,29 @@
+class Word
+    attr_reader :name, :id
+  
+    @@words = {}
+    @@id_tally = 0
+
+    def initialize(name, id)
+        @name = name
+        @id = id || @@id_tally += 1
+    end
+
+    def ==(other_word)
+        self.name.eql?(other_word.name)
+    end
+
+    def self.clear
+        @@words = {}
+        @@total_rows = 0
+    end
+
+    def self.all
+        @@words.values()
+    end
+    
+    def save
+        @@words[self.id] = Word.new(self.name, self.id)
+    end
+
+end
---
+++ spec/word_spec.rb
@@ -0,0 +1,44 @@
+require('rspec')
+require('word.rb')
+
+describe '#Word' do
+
+    before(:each) do
+        Word.clear()
+    end
+
+    describe("(1) #==") do
+        it("is the same word if it has the same attributes as another word") do
+          word1 = Word.new("Test 1", nil)
+          word2 = Word.new("Test 1", nil)
+          expect(word1).to(eq(word2))
+        end
+      end
+  
+      describe("(2) .all") do
+        it("returns an empty array when there are no words") do
+          expect(Word.all()).to(eq([]))
+        end
+      end
+      
+      describe("(3) #save") do
+        it("saves a word") do
+          word1 = Word.new("Test 3", nil) # nil added as second argument
+          word1.save()
+          word2 = Word.new("Test 3", nil) # nil added as second argument
+          word2.save()
+          expect(Word.all).to(eq([word1, word2]))
+        end
+      end
+    
+      describe("(4) .clear") do
+        it("clears all words") do
+          word1 = Word.new("Test 4", nil)
+          word1.save()
+          word2 = Word.new("Test 4", nil)
+          word2.save()
+          Word.clear()
+          expect(Word.all).to(eq([]))
+        end
+      end
+  end
