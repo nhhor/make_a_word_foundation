c4a9d60c01bbf6cc0cf84b07cb5c54daebbed38f
Noah Horwitz <nhhor@post.com>
2019-12-20 11:19:57 -0800

    troubleshoot spec 2, test works, app does not.

--- lib/word.rb
+++ lib/word.rb
@@ -1,33 +1,53 @@
 class Word
-    attr_reader :name, :id
-  
+    attr_reader :id, :name
+
     @@words = {}
-    @@id_tally = 0
+    @@word_tally = 0
 
     def initialize(name, id)
         @name = name
-        @id = id || @@id_tally += 1
-    end
-
-    def ==(other_word)
-        self.name.eql?(other_word.name)
-    end
-
-    def self.clear
-        @@words = {}
-        @@total_rows = 0
+        @id = id || @@word_tally += 1
     end
 
     def self.all
         @@words.values()
     end
 
-    def self.find(id)
-        @@words[id]
+    def self.search(x)
+        @@words.values().select {|e| /#{x}/i.match? e.name}
     end
-    
+
     def save
         @@words[self.id] = Word.new(self.name, self.id)
     end
 
+    def ==(word_to_compare)
+        self.name() == word_to_compare.name()
+    end
+
+    def self.clear
+        @@words = {}
+        @@word_tally = 0
+    end
+
+    def self.find(id)
+        @@words[id]
+    end
+
+    def update(name) #CHANGE
+        @name = (name != '') ? name : @name
+    end
+
+    def delete
+        @@words.delete(self.id)
+    end
+
+    def self.sort()
+        @@words.values().sort { |a, b| a.name <=> b.name }
+    end
+
+    def definitions
+        Definition.find_by_word(self.id)
+    end
+    
 end
